f373ef708eea5e711f3c496bbd07d17c9a74282a
Adetona Sunday <papasunnee@gmail.com>
2019-11-03 00:14:57 +0100

    gdfgdg

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,19 @@ const apolloServer = require('../apolloServer');
 
 const apiPath = '/graphql';
 
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+};
+
 // Setup Route Bindings
 module.exports = (app) => {
   // Views
-  app.get('/admin', (req, res) => { res.redirect('/keystone'); });
-  app.get('/', (req, res) => { res.redirect('/keystone'); });
-
-
-  apolloServer.applyMiddleware({ app, path: apiPath });
+  app.get('/admin', (req, res) => {
+    res.redirect('/keystone');
+  });
+  app.get('/', (req, res) => {
+    res.redirect('/keystone');
+  });
+  apolloServer.applyMiddleware({ app, path: apiPath, cors: corsOptions });
 };
